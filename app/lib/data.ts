5de224fff112383b0c87feaea2fcc6b5cfe557da
Simon Maluleka <Sicamaluks2014@gmail.com>
2023-11-15 21:05:09 +0200

    removed call to nostore()

--- app/lib/data.ts
+++ app/lib/data.ts
@@ -14,7 +14,6 @@ import { unstable_noStore as noStore } from "next/cache";
 export async function fetchRevenue() {
   // Add noStore() here prevent the response from being cached.
   // This is equivalent to in fetch(..., {cache: 'no-store'}).
-  noStore();
   try {
     // Artificially delay a reponse for demo purposes.
     // Don't do this in real life :)
@@ -34,7 +33,6 @@ export async function fetchRevenue() {
 }
 
 export async function fetchLatestInvoices() {
-  noStore();
   try {
     const data = await sql<LatestInvoiceRaw>`
       SELECT invoices.amount, customers.name, customers.image_url, customers.email, invoices.id
@@ -56,7 +54,6 @@ export async function fetchLatestInvoices() {
 }
 
 export async function fetchCardData() {
-  noStore();
   try {
     // You can probably combine these into a single SQL query
     // However, we are intentionally splitting them to demonstrate
@@ -96,7 +93,6 @@ export async function fetchFilteredInvoices(
   currentPage: number
 ) {
   const offset = (currentPage - 1) * ITEMS_PER_PAGE;
-  noStore();
   try {
     const invoices = await sql<InvoicesTable>`
       SELECT
@@ -127,7 +123,6 @@ export async function fetchFilteredInvoices(
 }
 
 export async function fetchInvoicesPages(query: string) {
-  noStore();
   try {
     const count = await sql`SELECT COUNT(*)
     FROM invoices
@@ -149,7 +144,6 @@ export async function fetchInvoicesPages(query: string) {
 }
 
 export async function fetchInvoiceById(id: string) {
-  noStore();
   try {
     const data = await sql<InvoiceForm>`
       SELECT
@@ -174,7 +168,6 @@ export async function fetchInvoiceById(id: string) {
 }
 
 export async function fetchCustomers() {
-  noStore();
   try {
     const data = await sql<CustomerField>`
       SELECT
@@ -193,7 +186,6 @@ export async function fetchCustomers() {
 }
 
 export async function fetchFilteredCustomers(query: string) {
-  noStore();
   try {
     const data = await sql<CustomersTable>`
 		SELECT
